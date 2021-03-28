9136e667e20a9d2b6fabda13c688a05de37355a6
Robbe Huybrechts <robbe.huybrechts@hotmail.com>
2021-03-28 13:08:05 +0200

    feat: add map borders, viewport, coins, size increase on collision

--- primus/server.js
+++ primus/server.js
@@ -1,15 +1,26 @@
 const Primus = require('primus');
 let primus;
 const players = [];
+const coins = [];
+const map = {
+  width: 2560,
+  height: 1920,
+}
 
 const go = server => {
     primus = Primus(server, {});
 
+    // Populate coins
+    for(let i = 0; i < 50; i++) {
+      coins.push({x: Math.random() * map.width, y: Math.random() * map.height});
+    }
+
     // Add player
     primus.on('connection', spark => {
       console.log('Player connected and adding at ID: ' + spark.id);
-      spark.write({type: 'id', id: spark.id});
-      players.push({id: spark.id});
+      spark.write({type: 'connect', id: spark.id, spark: spark});
+      spark.write({type: 'coins', coins: coins});
+      players.push({id: spark.id, spark: spark});
 
       // Update player
       spark.on('data', data => {
@@ -27,21 +38,46 @@ const go = server => {
       }), 1);
     });
 
-    setInterval(tick, 33);
+    setInterval(tick, 16);
 }
 
 const tick = _ => {
     primus.write({type: 'players', players: players});
-}
 
-var arr = [{ id: 1, username: 'fred' }, 
-  { id: 2, username: 'bill'}, 
-  { id: 3, username: 'ted' }];
+    // Check collision between player and coin
+    coins.forEach(coin => {
+      players.forEach(player => {
+        if(pointDistance(coin.x, coin.y, player.x, player.y) < player.size / 2) {
+          coin.x = Math.random() * map.width;
+          coin.y = Math.random() * map.height;
+
+          primus.forEach(function (spark, id, connections) {
+            if(spark.id == player.id) {
+              spark.write({type: 'sizeUpdate', amount: 5});
+            }
+          });
+
+          primus.write({type: 'coins', coins: coins});
+        }
+      });
+    });
+}
 
 const playerExists = (id) => {
   return players.some((el) => {
     return el.id === id;
   }); 
+}
+
+const getSparkById = (sparkId) => {
+
+}
+
+const pointDistance = (x1, y1, x2, y2) => {
+  let a = x1 - x2;
+  let b = y1 - y2;
+  
+  return Math.sqrt( a*a + b*b );
 }
 
 module.exports.go = go;
--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -13,25 +13,48 @@ const btnEnter = document.querySelector('#btnEnter');
 const resize = _ => {
     gameCanvas.width = window.innerWidth;
     gameCanvas.height = window.innerHeight;
+    view.width = window.innerWidth;
+    view.height = window.innerHeight;
+}
+
+const map = {
+    width: 2560,
+    height: 1920,
+}
+
+const player = {
+    id: 0,
+    spark: {},
+    name: '',
+    emoji: '',
+    x: 0,
+    y: 0,
+    size: 32,
+}
+
+view = {
+    width: 0,
+    height: 0,
+    x: player.x,
+    y: player.y,
 }
 
 resize();
 
-const player = {
-    id: 0,
-    name: 'Robinskie',
-    emoji: '🦝',
-    x: gameCanvas.width / 2 ,
-    y: gameCanvas.height / 2 ,
-}
-
-let players = []
+let players = [];
+let coins = [];
 
 keys = {
     up: false,
     right: false,
     down: false,
     left: false,
+}
+
+mouse = {
+    pressed: false,
+    x: 0,
+    y: 0,
 }
 
 addEventListener('keydown', e => {
@@ -68,10 +91,28 @@ addEventListener('keyup', e => {
     }
 });
 
+gameCanvas.addEventListener('mousedown', e => {
+    mouse.pressed = true;
+});
+
+gameCanvas.addEventListener('mousemove', e => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+});
+
+gameCanvas.addEventListener('mouseup', e => {
+    mouse.pressed = false;
+});
+
 c.textAlign = "center"; 
 c.textBaseline = "middle"; 
 
 const init = () => {
+    // Set pos
+    player.x = map.width / 2;
+    player.y = map.height / 2;
+
+    // Connect to server
     primus =  Primus.connect('', {
         reconnect: {
             max: Infinity // Number: The max delay before we try to reconnect.
@@ -82,8 +123,9 @@ const init = () => {
     
     primus.on('data', (data) => {
         // Receive initial id
-        if(data.type == 'id') {
+        if(data.type == 'connect') {
             player.id = data.id;
+            player.spark = data.spark;
 
             if(!GAME_STARTED) {
                 GAME_STARTED = true;
@@ -95,29 +137,67 @@ const init = () => {
         if(data.type == 'players') {
             players = data.players;
         }
+
+        // Receive coin data
+        if(data.type == 'coins') {
+            coins = data.coins;
+        }
+
+        // Receive size update
+        if(data.type == 'sizeUpdate') {
+            player.size += data.amount;
+        }
     });
 }
 
 loop = _ => {
-    const PLAYER_SPEED = 2;
+    const PLAYER_SPEED = 4;
+    let MOVING = false;
 
     // UPDATE
+    // Keyboard
+    if(keys.left || keys.right || keys.up || keys.down) MOVING = true;
     if(keys.right) player.x += PLAYER_SPEED;
     if(keys.left) player.x -= PLAYER_SPEED;
     if(keys.down) player.y += PLAYER_SPEED;
     if(keys.up) player.y -= PLAYER_SPEED;
 
+    // Mouse
+    /*
+    if(!MOVING && mouse.pressed) {
+        player.x += Math.sign(mouse.x - view.x / 2 - player.x) * PLAYER_SPEED;
+        player.y += Math.sign(mouse.y - view.y / 2 - player.y) * PLAYER_SPEED;
+    }
+    */
+
+    // Borders
     if(player.x < 0) player.x = 0;
-    if(player.x > gameCanvas.width) player.x = gameCanvas.width;
+    if(player.x > map.width) player.x = map.width;
     if(player.y < 0) player.y = 0;
-    if(player.y > gameCanvas.height) player.y = gameCanvas.height;
+    if(player.y > map.height) player.y = map.height;
 
     // SEND TO SERVER
     primus.write({type: 'player', player: player});
 
-    // RENDER
-    // Clear screen
+    // CLEAR SCREEN
     c.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+    // UPDATE VIEW
+    view.x = player.x;
+    view.y = player.y;
+    c.save();
+    c.translate(-view.x + view.width / 2, -view.y + view.height / 2);
+    // Update background
+    gameCanvas.style.backgroundPosition = `${-view.x}px ${-view.y}px`;
+    
+    // RENDER
+    // Draw borders
+    c.strokeRect(0, 0, map.width, map.height);
+
+    // Draw coins
+    coins.forEach(coin => {
+        drawCoin(coin);
+    })
 
     // Draw self
     drawPlayer(player);
@@ -127,14 +207,32 @@ loop = _ => {
         if(otherPlayer.id != player.id)
             drawPlayer(otherPlayer);
     })
+
+    // RESTORE VIEW
+    c.restore();
 }
 
+// Draw functions
 const drawPlayer = player => {
-    c.font = '18px sans-serif';
-    c.fillText(player.name, player.x, player.y - 32);
+    // Name
+    c.font = `18px sans-serif`;
+    c.fillText(player.name, player.x, player.y - player.size * 0.75 - 8);
 
-    c.font = '32px sans-serif';
+    // Emoji
+    c.font = `${player.size}px sans-serif`;
     c.fillText(player.emoji, player.x, player.y);
+}
+
+const drawCoin = coin => {
+    c.font = `24px sans-serif`;
+    c.fillText('💲', coin.x, coin.y);
+}
+
+const drawLine = (x1, y1, x2, y2) => {
+    c.moveTo(x1, y1);
+    c.beginpath();
+    c.lineTo(x2, y2);
+    c.stroke();
 }
 
 btnEnter.addEventListener('click', _ => {
